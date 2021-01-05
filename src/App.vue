efeaed09d0037801efa4308378acfc6d3eaf6d93
Arman Shojaei <amnshojaei@gmail.com>
2021-01-06 01:19:49 +0330

    chore: show result after exam done

--- src/App.vue
+++ src/App.vue
@@ -1,11 +1,32 @@
 <template>
-  <div>
-    <h1>Hello</h1>
+  <div class="wrapper">
+    <app-result v-if="isExamDone" />
+    <app-steps v-else @finish="checkAnswers" />
   </div>
 </template>
 
 <script>
 export default {
-  name: 'App'
+  name: 'App',
+  components: {
+    AppSteps: () => import('@/components/Steps'),
+    AppResult: () => import('@/components/Result')
+  },
+  data () {
+    return {
+      isExamDone: false
+    }
+  },
+  methods: {
+    /**
+     * Get answer array and
+     * set it to local storage.
+     * Show result component.
+     */
+    checkAnswers (payload) {
+      localStorage.setItem('answers', payload)
+      this.isExamDone = true
+    }
+  }
 }
 </script>
