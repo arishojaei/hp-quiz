4d877867777194b86423b8c6e9cac058fea31325
Arman Shojaei <amnshojaei@gmail.com>
2021-01-06 01:10:25 +0330

    feat: add option list

---
+++ src/components/Options.vue
@@ -0,0 +1,46 @@
+<template>
+  <div class="options">
+    <div
+      class="_option"
+      v-for="(opt, optIndex) in options"
+      :key="optIndex"
+    >
+      <input
+        type="radio"
+        name="options"
+        :id="`opt${optIndex}`"
+        :value="optIndex"
+        v-model="selectedOption"
+      >
+      <label :for="`opt${optIndex}`">
+        {{ opt.text }}
+      </label>
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'Option',
+  props: {
+    /**
+     * List of options
+     * to choose
+     */
+    options: {
+      type: Array,
+      required: true
+    }
+  },
+  data () {
+    return {
+      selectedOption: ''
+    }
+  },
+  watch: {
+    selectedOption (value) {
+      this.$emit('answerSelect', value)
+    }
+  }
+}
+</script>
